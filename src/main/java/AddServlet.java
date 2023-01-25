4612cb0590f9b95f7e88eda93c43451f29a595d0
madar1992 <96854154+madar1992@users.noreply.github.com>
2023-01-25 11:22:23 +0530

    Add files via upload

---
+++ src/main/java/AddServlet.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet
+{
+   public void service(HttpServletRequest req, HttpServletResponse res) {
+	   String a=req.getParameter("t1");
+	   String b=req.getParameter("t2");
+	   
+	   double x=Double.parseDouble(a);
+	   double y=Double.parseDouble(b);
+	   
+	   double c=x+y;
+	   System.out.println(c);
+	   
+	   try {
+		PrintWriter pw=res.getWriter();
+		pw.println("<body><h1> here is the sum of two numbers "+c+"</h1><body>");
+		pw.println("<a href=\"index.html\">Go Back</a>");
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   
+   }
+   
+}
